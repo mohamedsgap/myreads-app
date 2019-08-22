4fe59df4fbd0efaecf8667e922ec6a5632f398cc
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-22 11:13:08 +0200

    fix edge cases

--- src/App.js
+++ src/App.js
@@ -160,13 +160,16 @@ const Book = props => {
             style={{
               width: 128,
               height: 193,
-              backgroundImage: `url(${book.imageLinks.thumbnail})`,
+              backgroundImage: `url(${book.imageLinks &&
+                book.imageLinks.thumbnail})`,
             }}
           />
           <BookshelfChanger book={book} shelf={shelf} onMove={onMove} />
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors.join(', ')}</div>
+        <div className="book-authors">
+          {book.authors && book.authors.join(', ')}
+        </div>
       </div>
     </li>
   );
