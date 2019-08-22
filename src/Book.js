e3f6085599edcd32772214026cc1eafdbdf40496
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-22 12:37:53 +0200

    refacotr: Book component

--- src/Book.js
+++ src/Book.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import BookshelfChanger from './BookshelfChanger';
 
-const Book = props => {
-  const { book, shelf, onMove } = props;
-  return (
-    <li>
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage: `url(${book.imageLinks &&
-                book.imageLinks.thumbnail})`,
-            }}
+
+
+const Book = ({ book, shelf, onMove }) => (
+  <li>
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url(${
+              book.imageLinks
+                ? book.imageLinks.thumbnail
+                : 'icons/book-placeholder.svg'
+            })`
+          }}
           />
           <BookshelfChanger book={book} shelf={shelf} onMove={onMove} />
         </div>
         <div className="book-title">{book.title}</div>
         <div className="book-authors">
-          {book.authors && book.authors.join(', ')}
+        {book.authors ? book.authors.join(', ') : 'Unknown Author'}
         </div>
       </div>
-    </li>
-  );
-};
+  </li>
+);
 
 export default Book;
