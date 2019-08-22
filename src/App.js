13c4cb15c2b779a0c6a9fb7e0994ab0b44cbbd01
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-22 09:51:05 +0200

    pass props into components

--- src/App.js
+++ src/App.js
@@ -2,28 +2,46 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import { Route, Link } from 'react-router-dom'
 import './App.css'
+import getAll from './data';
+
 
 class BooksApp extends React.Component{
-  state = { };
-  render () {
+  
+  bookshelves = [
+    { key: 'currentlyReading', name: 'Currently Reading' },
+    { key: 'wantToRead', name: 'Want to Read' },
+    { key: 'read', name: 'Have Read' },
+  ];
+
+  state = {
+    books: getAll,
+   };
+   render() {
+    const { books } = this.state;
     return (
-      <div className='app'>
-        <Route exact path='/' component={ListBooks} />
-        <Route path='/search' component={BookSearch} />
+      <div className="app">
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListBooks bookshelves={this.bookshelves} books={books} />
+          )}
+        />
+        <Route path="/search" render={() => <SearchBooks books={books} />} />
       </div>
-    )
+    );
   }
 }
 
 class ListBooks extends React.Component {
   render() {
-    const { bookshelves } = this.props;
+    const { bookshelves, books } = this.props;
     return (
       <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
-        <Bookcase bookshelves={bookshelves} />
+        <Bookcase bookshelves={bookshelves} books={books} />
         <OpenSearchButton />
       </div>
     );
@@ -41,12 +59,12 @@ const OpenSearchButton = () => {
 };
 
 const Bookcase = props => {
-  const { bookshelves } = props;
+  const { bookshelves, books } = props;
   return (
     <div className="list-books-content">
       <div>
         {bookshelves.map(shelf => (
-          <Bookshelf key={shelf.key} shelf={shelf} />
+          <Bookshelf key={shelf.key} shelf={shelf} books={books} />
         ))}
       </div>
     </div>
@@ -54,13 +72,16 @@ const Bookcase = props => {
 };
 
 const Bookshelf = props => {
-  const { shelf } = props;
+  const { shelf, books } = props;
+  const booksOnThisShelf = books.filter(book => book.shelf === shelf.key);
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{shelf.name}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          <Book book={{}} />
+          {booksOnThisShelf.map(book => (
+            <Book key={book.id} book={book} shelf={shelf.key} />
+          ))}
         </ol>
       </div>
     </div>
@@ -68,7 +89,7 @@ const Bookshelf = props => {
 };
 
 const Book = props => {
-  const { book } = props;
+  const { book, shelf } = props;
   return (
     <li>
       <div className="book">
@@ -78,14 +99,13 @@ const Book = props => {
             style={{
               width: 128,
               height: 193,
-              backgroundImage:
-                'url("http://books.google.com/books/content?id=PGR2Aw...")',
+              backgroundImage: `url(${book.imageLinks.thumbnail})`,
             }}
           />
-          <BookshelfChanger />
+          <BookshelfChanger book={book} shelf={shelf} />
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors.join(', ')}</div>
       </div>
     </li>
   );
@@ -95,7 +115,7 @@ class BookshelfChanger extends React.Component {
   render() {
     return (
       <div className="book-shelf-changer">
-        <select>
+        <select value={this.props.shelf}>
           <option value="move" disabled>
             Move to...
           </option>
