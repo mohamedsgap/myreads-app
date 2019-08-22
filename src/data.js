f2eb9a6e7459da8f0ef3dfffe72c99a27a0f5ef0
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-08-22 09:54:53 +0200

    create static data

---
+++ src/data.js
@@ -0,0 +1,15 @@
+const getAll = [
+    {
+      title: 'The Linux Command Line',
+      subtitle: 'A Complete Introduction',
+      // more data...
+    },
+    {
+      title: 'The React Handbook',
+      subtitle: 'Learning to Build Dynamic UIs',
+      // more data...
+    },
+    // next book...
+  ];
+  
+  export default getAll;
